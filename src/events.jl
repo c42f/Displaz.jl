90e083d18f9cd0016fb5ea5bafb2e963d1895871
Chris Foster <chris.foster@roames.com.au>
2016-06-23 08:21:32 -0400

    Some documentation for event_loop

--- src/events.jl
+++ src/events.jl
@@ -20,6 +20,31 @@ _argspec_str(s::AbstractString) = s
 _argspec_str(::Type{Void}) = "null"
 _argspec_str(::Type{CursorPosition}) = "cursor"
 
+
+"""
+    event_loop(callback::Function, event_list...)
+
+Subscribe to a list of events, calling `callback` each time one is received.
+
+Each event comes with some optional some state which is attached at the time
+the event is triggered.  The events are specified as a list of `event=>state`
+pairs.
+
+Currently only `KeyEvent` is supported, with the possible arguments `Void` or
+`CursorPosition`.  For example:
+
+```
+Displaz.event_loop(
+        KeyEvent("c")=>Void,
+        KeyEvent("p")=>CursorPosition
+) do event, arg
+    @show event, arg
+    if event == KeyEvent("c")
+        clearplot()
+    end
+end
+```
+"""
 function event_loop(callback::Function, event_list...)
     hookopts = [["-hook", _eventspec_str(e), _argspec_str(p)]
                 for (e,p) in event_list]
