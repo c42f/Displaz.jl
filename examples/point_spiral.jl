6e0c56a93b9b96eef041c2f15f8c07ffc3446bc4
Martijn Visser <mgvisser@gmail.com>
2019-02-09 22:22:44 +0100

    update to julia 1.0
    Updated julia syntax and fixed deprecation warnings. With this locally I can run the `point_spiral.jl` example (pretty!) and the README code.

--- examples/point_spiral.jl
+++ examples/point_spiral.jl
@@ -1,13 +1,13 @@
 using Displaz
 
 function example(N)
-    t = linspace(0,1, N)'
-    P = (10.+0.2*rand(size(t))) .* vcat(exp(5*t).*cos(100*pi*t),
-                                        exp(5*t).*sin(100*pi*t),
-                                        100*t);
-    C = vcat(t, 1.-t, zeros(t));
+    t = range(0, stop=1, length=N)'
+    P = (10 .+ 0.2*rand(Float64, size(t))) .* vcat(exp.(5*t).*cos.(100*pi*t),
+                                                   exp.(5*t).*sin.(100*pi*t),
+                                                   100*t);
+    C = vcat(t, 1 .- t, zero(t));
     sz  = map(Float32, t)
-    shape = mod(rand(UInt8, N), 6)
+    shape = mod.(rand(UInt8, N), 6)
     plot3d!(P, color = C, label="Top series", markershape=shape)
     P[3,:] = -P[3,:]
     plot3d!(P, color = C, markersize = sz, markershape="-", label="Bottom series")
